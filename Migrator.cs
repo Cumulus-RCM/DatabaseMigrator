701698d421e58cbc339ad8e3bdce4aa7d59f7ac1
stokara <avistokar@gmail.com>
2023-05-19 14:03:58 -0400

    removed seperate class made script retrieval async

--- Migrator.cs
+++ Migrator.cs
@@ -7,7 +7,8 @@ namespace DatabaseMigrator;
 public class Migrator {
     private readonly DbConnectionManager connectionManager;
     private readonly ILogger<Migrator> logger;
-    private const string APPLIED_MIGRATION_SCRIPT_SQL = 
+
+    private const string APPLIED_MIGRATION_SCRIPT_SQL =
         @"IF OBJECT_ID(N'dbo.AppliedMigrationScript', 'U') IS NULL
    CREATE TABLE dbo.AppliedMigrationScript (
       ScriptName varchar(500) NOT NULL,
@@ -20,22 +21,22 @@ SELECT ScriptName FROM AppliedMigrationScript";
         this.logger = loggerFactory.CreateLogger<Migrator>();
     }
 
-    public async Task<bool> UpgradeDatabaseAsync() {
+    public async Task<bool> UpgradeDatabaseAsync(string scriptPath) {
         using var conn = connectionManager.CreateConnection();
-        var appliedScriptNames = await conn.QueryAsync<string>(APPLIED_MIGRATION_SCRIPT_SQL).ConfigureAwait(false);
-        var path = Path.Combine( AppDomain.CurrentDomain.BaseDirectory, "MigrationScripts");
-        var migrationScripts = getScripts(path, appliedScriptNames.ToList());
+        var appliedScriptNames = (await conn.QueryAsync<string>(APPLIED_MIGRATION_SCRIPT_SQL).ConfigureAwait(false)).ToList();
+        var migrationScripts = await getScriptsAsync(scriptPath, appliedScriptNames).ConfigureAwait(false);
         if (!migrationScripts.Any()) return true;
-                  
+
         using var transaction = conn.BeginTransaction();
         var currentScriptName = "";
         try {
             foreach (var migrationScript in migrationScripts) {
                 currentScriptName = migrationScript.scriptName;
-                await conn.ExecuteAsync(migrationScript.script, transaction:transaction).ConfigureAwait(false);
+                await conn.ExecuteAsync(migrationScript.script, transaction: transaction).ConfigureAwait(false);
                 await conn.ExecuteAsync("INSERT INTO AppliedMigrationScript (ScriptName) VALUES (@scriptName)", new {migrationScript.scriptName}, transaction).ConfigureAwait(false);
                 logger.LogInformation($"Applied Database Migration Script: {migrationScript.scriptName}.");
             }
+
             transaction.Commit();
             return true;
         }
@@ -44,12 +45,16 @@ SELECT ScriptName FROM AppliedMigrationScript";
             logger.LogError(e, $"Error Applying Migration Script {currentScriptName}.");
             return false;
         }
-    }
 
-    private static ICollection<(string scriptName, string script)> getScripts(string path, ICollection<string> appliedScriptNames) {
-        var scripts = MigrationScripts.GetScripts(path);
-        return scripts
-            .Where(s => appliedScriptNames.All(appliedScriptName => appliedScriptName != s.scriptName))
-            .ToList();
+        static async Task<ICollection<(string scriptName, string script)>> getScriptsAsync(string migrationScriptPath, ICollection<string> alreadyAppliedScripts) {
+            var scriptNames = Directory.EnumerateFiles(migrationScriptPath, "*.sql")
+                .Where(filename => !alreadyAppliedScripts.Contains(filename))
+                .OrderBy(filename => filename);
+            var result = new List<(string, string)>();
+            foreach (var scriptName in scriptNames) {
+                result.Add((scriptName, await File.ReadAllTextAsync(scriptName)));
+            }
+            return result;
         }
+    }
 }

0fff6f70c5def4c35a9e0a71f8f19bbc335a910a
stokara <avistokar@gmail.com>
2023-06-30 14:26:24 -0400

    DatabaseMigrator uses Json instead of sql files

--- Migrator.cs
+++ Migrator.cs
@@ -1,4 +1,5 @@
-﻿using BaseLib;
+﻿using System.Text.Json;
+using BaseLib;
 using Dapper;
 using Microsoft.Extensions.Logging;
 
@@ -11,31 +12,34 @@ public class Migrator {
     private const string APPLIED_MIGRATION_SCRIPT_SQL = 
         @"IF OBJECT_ID(N'dbo.AppliedMigrationScript', 'U') IS NULL
    CREATE TABLE dbo.AppliedMigrationScript (
-      ScriptName varchar(500) NOT NULL,
+      Script_Id int NOT NULL,
       AppliedOn smalldatetime NOT NULL DEFAULT (getutcdate()),
-   CONSTRAINT PK_AppliedMigrationScript_ScriptName PRIMARY KEY CLUSTERED (ScriptName));
-SELECT ScriptName FROM AppliedMigrationScript";
+   CONSTRAINT PK_AppliedMigrationScript PRIMARY KEY CLUSTERED (Script_Id));
+SELECT Script_Id FROM AppliedMigrationScript";
 
     public Migrator(IDbConnectionManager connectionManager, ILoggerFactory loggerFactory) {
         this.connectionManager = connectionManager;
         this.logger = loggerFactory.CreateLogger<Migrator>();
     }
 
+    // ReSharper disable PossibleMultipleEnumeration
     public async Task<bool> UpgradeDatabaseAsync() {
         using var conn = connectionManager.CreateConnection();
-        var appliedScriptNames = (await conn.QueryAsync<string>(APPLIED_MIGRATION_SCRIPT_SQL).ConfigureAwait(false)).ToList();
-        var path = Path.Combine( AppDomain.CurrentDomain.BaseDirectory, "MigrationScripts");
-        var migrationScripts = await getScriptsAsync(path, appliedScriptNames);
+        var appliedScripts = await conn.QueryAsync<int>(APPLIED_MIGRATION_SCRIPT_SQL).ConfigureAwait(false);
+        var path = Path.Combine( AppDomain.CurrentDomain.BaseDirectory, "MigrationScripts\\MigrationScripts.json");
+        if (!File.Exists(path)) return true;
+        var json = await File.ReadAllTextAsync(path);
+        var migrationScripts = JsonSerializer.Deserialize<IEnumerable<MigrationScript>>(json)!.Where(ms => ms.IsActive && !appliedScripts.Contains(ms.Id));
         if (!migrationScripts.Any()) return true;
 
         using var transaction = conn.BeginTransaction();
         var currentScriptName = "";
         try {
-            foreach (var migrationScript in migrationScripts) {
-                currentScriptName = migrationScript.scriptName;
-                await conn.ExecuteAsync(migrationScript.script, transaction: transaction).ConfigureAwait(false);
-                await conn.ExecuteAsync("INSERT INTO AppliedMigrationScript (ScriptName) VALUES (@scriptName)", new {migrationScript.scriptName}, transaction).ConfigureAwait(false);
-                logger.LogInformation($"Applied Database Migration Script: {migrationScript.scriptName}.");
+           
+            foreach (var migrationScript in migrationScripts.Where(ms=>ms.IsActive).OrderBy(ms=>ms.ScriptOrder)) {
+                await conn.ExecuteAsync(migrationScript.Script, transaction: transaction).ConfigureAwait(false);
+                await conn.ExecuteAsync("INSERT INTO AppliedMigrationScript (Script_Id) VALUES (@Id)", new {migrationScript.Id}, transaction).ConfigureAwait(false);
+                logger.LogInformation($"Applied Database Migration Script: {migrationScript.Id}, {migrationScript.Description}.");
             }
 
             transaction.Commit();
@@ -46,16 +50,6 @@ SELECT ScriptName FROM AppliedMigrationScript";
             logger.LogError(e, $"Error Applying Migration Script {currentScriptName}.");
             return false;
         }
-
-        static async Task<ICollection<(string scriptName, string script)>> getScriptsAsync(string migrationScriptPath, ICollection<string> alreadyAppliedScripts) {
-            var scriptNames = Directory.EnumerateFiles(migrationScriptPath, "*.sql")
-                .Where(filename => !alreadyAppliedScripts.Contains(filename))
-                .OrderBy(filename => filename);
-            var result = new List<(string, string)>();
-            foreach (var scriptName in scriptNames) {
-                result.Add((scriptName, await File.ReadAllTextAsync(scriptName)));
-            }
-            return result;
-        }
     }
+    // ReSharper restore PossibleMultipleEnumeration
 }
--- Models/MigrationScript.cs
+++ Models/MigrationScript.cs
@@ -4,6 +4,7 @@ public class MigrationScript {
     public int Id { get; set; }
     public string Description { get; set; } = "";
     public string Script { get; set; } = "";
-    public uint ScriptOrder { get; set; }
+    public int ScriptOrder { get; set; }
+    public bool IsActive { get; set; }
     public DateTime Created { get; set; } = DateTime.Now;
 }
